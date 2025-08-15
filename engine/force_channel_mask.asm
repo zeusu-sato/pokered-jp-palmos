61e0dd69b560ecdfb2ad5e387fef9c495474719d
Zeusu Sato <zeusu.sato@dorodango.biz>
2025-08-15 17:47:53 +0900

    Add strict mute mode for channel masking

--- engine/force_channel_mask.asm
+++ engine/force_channel_mask.asm
@@ -1,13 +1,27 @@
+INCLUDE "engine/channel_mask.inc"
+
 IF DEF(CH_MIXING)
     ; 互換のため空
-ENDC
-
-IF !DEF(CH_MASK)
-    DEF CH_MASK EQU $22  ; 既定: CH2のみ L/R
 ENDC
 
 SECTION "ChannelMaskRoutine", ROM0
 ForceChannelMask::
     ld  a, CH_MASK
     ldh [rNR51], a
+    ret
+
+; EnforceStrictMute: 非許可chの音量/DACを毎フレーム落とす
+EnforceStrictMute::
+IF !ALLOW_CH1
+    xor a        ; a=0
+    ldh [rNR12], a   ; CH1 volume=0
+ENDC
+IF !ALLOW_CH3
+    xor a
+    ldh [rNR30], a   ; CH3 DAC off
+ENDC
+IF !ALLOW_CH4
+    xor a
+    ldh [rNR42], a   ; CH4 volume=0
+ENDC
     ret
